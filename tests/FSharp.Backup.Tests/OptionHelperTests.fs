ab50a52cf156661ea136994e96dc0779a530ef91
Rex Ng <rexng@thisisnoble.com>
2016-12-19 14:13:12 +0800

    Add tests to OptionHelper

---
+++ tests/FSharp.Backup.Tests/OptionHelperTests.fs
@@ -0,0 +1,19 @@
+﻿module FSharp.Backup.Tests.OptionHelperTests
+
+open Option
+open FsCheck.Xunit
+open Swensen.Unquote
+
+[<Property>]
+let ``Either returns same underlying value when given id`` (x: int) none =
+    let xOpt = Some x
+    
+    let result = either id none xOpt
+
+    x =! result
+
+[<Property>]
+let ``Either returns none value when input option is None`` (none: int) =
+    let result = either id none None
+
+    result =! none
