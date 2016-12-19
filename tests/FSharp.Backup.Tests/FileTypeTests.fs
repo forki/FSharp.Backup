620b15239a48221642f357a79c05bd1e9e70cfcc
Rex Ng <rexng@thisisnoble.com>
2016-12-19 13:49:57 +0800

    Add extra tests

--- tests/FSharp.Backup.Tests/FileTypeTests.fs
+++ tests/FSharp.Backup.Tests/FileTypeTests.fs
@@ -7,7 +7,25 @@ open FsCheck.Xunit
 open Swensen.Unquote
 
 [<Property>]
-let ``File can be constructed`` (NonNull s) =
+let ``File can be constructed without extension`` (NonNull s) =
     let fileName = FileName s
     let file = File (fileName, None)
+
     sprintf "%A" file =! s
+
+[<Property>]
+let ``File can be constructed with extension`` (NonNull fileNameString) (NonNull fileExtensionString) =
+    let fileName = FileName fileNameString
+    let fileExtension = FileExtension fileExtensionString
+
+    let file = File (fileName, Some fileExtension)
+
+    sprintf "%A" file =! sprintf "%s.%s" fileNameString fileExtensionString
+
+[<Property>]
+let ``Directory returns correct string representation`` (NonNull dirNameString) f =
+    let dirName = DirectoryName dirNameString
+
+    let dir = Directory (dirName, f)
+
+    sprintf "%A" dir =! sprintf "Directory: %s" dirNameString
